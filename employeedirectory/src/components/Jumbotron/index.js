289dc5e7f7280309e07b6cf6ec1ad73e27a1e18b
marisanesmith <marisa@thisworldtraveled.com>
2021-04-22 23:04:33 -0400

    updated jumbotron index.js

--- employeedirectory/src/components/Jumbotron/index.js
+++ employeedirectory/src/components/Jumbotron/index.js
@@ -5,7 +5,7 @@ function Jumbotron() {
     return (
     <div className="jumbotron">
         <h1 className="display-4">Employee Directory</h1>
-        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p className="lead">A list of all the employees at the comapny. Use the search box to narrow your results.</p>
     </div>
   );
 }

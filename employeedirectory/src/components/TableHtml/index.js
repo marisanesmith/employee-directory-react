fdc49d7db3f35ce368f3647669c47aa192986377
marisanesmith <marisa@thisworldtraveled.com>
2021-04-24 16:04:28 -0400

    updated api.js

--- employeedirectory/src/components/TableHtml/index.js
+++ employeedirectory/src/components/TableHtml/index.js
@@ -10,10 +10,12 @@ function TableHtml() {
         <table className="table table-striped">
         <thead>
           <tr>
-            <th scope="col">#</th>
+            <th scope="col">Image</th>
             <th scope="col">First</th>
             <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+            <th scope="col">DOB</th>
           </tr>
         </thead>
         <tbody>
--- employeedirectory/src/utils/api.js
+++ employeedirectory/src/utils/api.js
@@ -1,5 +1,20 @@
 
 // the URL and information for the api call, axios
 
-import Axios from "axios";
+import axios from "axios";
+const BASEURL = 'https://randomuser.me/api/';
+
+export default{
+    getUsers:function() {
+        return axios.get('https://randomuser.me/api/')
+    }
+}
+
+$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+    }
+  });
 

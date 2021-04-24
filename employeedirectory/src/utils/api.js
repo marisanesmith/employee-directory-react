e01ab7fef1988f05fa771204be38db289523fed6
marisanesmith <marisa@thisworldtraveled.com>
2021-04-24 16:18:26 -0400

    api key

--- employeedirectory/src/utils/api.js
+++ employeedirectory/src/utils/api.js
@@ -2,19 +2,12 @@
 // the URL and information for the api call, axios
 
 import axios from "axios";
-const BASEURL = 'https://randomuser.me/api/';
+
+//gets all users
 
 export default{
-    getUsers:function() {
-        return axios.get('https://randomuser.me/api/')
+    search:function() {
+        return axios.get('https://randomuser.me/api/?results=10&inc=picture,name,email,phone,nat&nat=NO')
     }
 }
-
-$.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function(data) {
-      console.log(data);
-    }
-  });
 
